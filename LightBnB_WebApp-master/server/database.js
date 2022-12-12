0c66ea26e79384f7b09aa31f3122080506aaffa9
Hillary Forget <hillaryforget@outlook.com>
2022-12-12 13:55:50 -0500

    lightbnb web boilerplate, refactor getallproperties

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -75,15 +75,19 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+const getAllProperties = (options, limit = 10) => {
+  return pool
+    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
-exports.getAllProperties = getAllProperties;
 
+exports.getAllProperties = getAllProperties;
 
 /**
  * Add a property to the database
