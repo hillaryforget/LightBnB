d51340d760abfe506e5fba436e5fc8d31609c814
Hillary Forget <hillaryforget@outlook.com>
2022-12-13 22:18:38 -0500

    refactor users to use lightbnb database and SQL

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -10,24 +10,18 @@ const pool = new Pool({
   database: 'lightbnb'
 });
 
-/// Users
+/// Users refactored to use lightbnb database and SQL queries
 
 /**
  * Get a single user from the database given their email.
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+const getUserWithEmail = (email) => {
+  return pool
+    .query(`SELECT * FROM users WHERE email = $1;`, [email])
+    .then(res => res.rows[0])
+    .catch(err => null);
 };
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -36,22 +30,23 @@ exports.getUserWithEmail = getUserWithEmail;
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+const getUserWithId = (id) => {
+  return pool
+    .query(`SELECT * FROM users WHERE id = $1`, [id])
+    .then(res => res.rows[0])
+    .catch(err => null);
 };
 exports.getUserWithId = getUserWithId;
-
 
 /**
  * Add a new user to the database.
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+const addUser = (user) => {
+  return pool
+    .query(`INSERT INTO users (name, email, password) VALUES($1, $2, $3) RETURNING *`, [user.name, user.email, user.password])
+    .then(res => res.rows[0]);
 };
 exports.addUser = addUser;
 
@@ -67,7 +62,7 @@ const getAllReservations = function(guest_id, limit = 10) {
 };
 exports.getAllReservations = getAllReservations;
 
-/// Properties
+/// Properties refactored to use lightbnb database and SQL queries
 
 /**
  * Get all properties.
